aace845960efe891a659e0d4573976bd5e048efd
brenomarinho2001 <74988159+brenomarinho2001@users.noreply.github.com>
2023-03-30 20:36:26 -0300

    erros

--- script.js
+++ script.js
@@ -15,18 +15,57 @@ function getPokemon(nome){
 
     var n = nome.toLowerCase()
     getData()
+
+
+    // Zerando Variáveis 
+    var errors = document.getElementById("error");
+    errors.innerHTML = 'loading..   '
+
+    var imagemfront = document.getElementById("imgpokemon");
+    imagemfront.src = ''
+
+    var imagemback = document.getElementById("imgpokemon2");
+    imagemback.src = ''
+
+    var nome_pokemon = document.getElementById("nomepokemon");
+    nome_pokemon.innerHTML = ''
+
+
     async function getData() {
         try {
+
+          //ACHEI UM POKEMON
+          var errors = document.getElementById("error");
+          errors.innerHTML = ''
           const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+n);
           const data = await response.json();
 
         
           var nome_pokemon = document.getElementById("nomepokemon");
           nome_pokemon.innerHTML = data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1);
-          var imagem = document.getElementById("imgpokemon"); // seleciona a imagem pelo id
-          imagem.src = data.sprites.front_default;      
+
+          var imagemfront = document.getElementById("imgpokemon"); // seleciona a imagem pelo id
+          imagemfront.src = data.sprites.front_default;      
+
+          var imagemback = document.getElementById("imgpokemon2"); // seleciona a imagem pelo id
+          imagemback.src = data.sprites.back_default
+
+          var errors = document.getElementById("error");
+          errors.innerHTML = ''
+          console.log(data)
         } catch (error) {
-          console.error(error);
+          //NÃO ACHEI O POKEMON ESPECIFICADO!
+          var imagemfront = document.getElementById("imgpokemon");
+          imagemfront.src = ''
+
+          var imagemback = document.getElementById("imgpokemon2");
+          imagemback.src = ''
+
+          var nome_pokemon = document.getElementById("nomepokemon");
+          nome_pokemon.innerHTML = ''
+
+          var errors = document.getElementById("error");
+          errors.innerHTML = 'Não encontramos o Pokemon!'
         }
       }
 
